41fc197bd77bb8842cff2ffb35b8b98bc02f2616
Michal Adryanczyk <miadry@gmail.com>
2023-07-05 22:19:41 +0200

    HW-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+
+const galleryItemsMap = galleryItems
+	.map((item) => {
+		galleryList.insertAdjacentHTML(
+			"beforeend",
+			`<div class="gallery__item">
+			<a class="gallery__link" href="${item.original}">
+			<img 
+			class="gallery__image"
+			src="${item.preview}"
+      		data-source="${item.original}"
+			alt="${item.description}"
+			/>
+			</a>
+			</div`
+			);
+		})
+		.join("");
+        
+        new SimpleLightbox('.gallery a', {
+            captionsData: 'alt',
+            captionDelay: 250,
+        });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const LocalStorageData = 'feedback-form-state';
+const formElement = document.querySelector('.feedback-form');
+const { email: emailInput, message: messageInput } = formElement.elements;
+const lastFormState = localStorage.getItem(LocalStorageData);
+
+const storageInput = () => {
+  formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem(LocalStorageData, JSON.stringify(formState));
+};
+
+const onSubmit = event => {
+  formElement.reset();
+  localStorage.removeItem(LocalStorageData);
+  console.log(formState);
+  event.preventDefault();
+};
+
+let formState = null;
+
+if (lastFormState) {
+  try {
+    formState = JSON.parse(lastFormState);
+    messageInput.value = formState.message;
+    emailInput.value = formState.email;
+  } catch (error) {
+    console.error(`${error.name}: ${error.message}.`);
+  }
+}
+formElement.addEventListener('input', throttle(storageInput, 500));
+formElement.addEventListener('submit', onSubmit);
